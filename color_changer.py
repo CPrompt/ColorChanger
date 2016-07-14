1ba6a7552c93bbfffe5dfaf7b224fa4a9d6a3f5f
Curtis Adkins <curtis@wayne-manor.gotham>
2016-07-13 21:39:54 -0400

    One class to rule them all

--- color_changer.py
+++ color_changer.py
@@ -6,13 +6,14 @@
 #
 
 import json
+import os
 
-theme_data = ""
 theme_file = ""
 color_value = None
 default_color_value = "#ffffff"
 
 class Colors:
+
 
     # read the json file
     def read_json(self,theme_file):
@@ -22,37 +23,25 @@ class Colors:
         with open(theme_file) as data_file:
             data = json.load(data_file)
 
-            return(data)
+        return(data)
 
     # ask for key and return value
     # if no key is found, return default value
     def get_key_value(self,color_key):
 
+        #print(os.path.dirname(os.path.realpath(self.theme_file)))
+        theme_data = self.read_json(self.theme_file)
+
         if color_key in theme_data:
             color_value = theme_data.get(color_key)
         else:
             color_value = default_color_value
-
         return(color_value)
 
 
+objColor = Colors()
+print(objColor.read_json('theme1.json'))
+print(objColor.get_key_value('color3'))
 
 
 
-#objColors = Colors()
-
-# read the entire datafile
-#theme_data = objColors.read_json("theme1.json")
-
-'''
-def get_key_value(color_key):
-
-    if color_key in theme_data:
-        color_value = theme_data.get(color_key)
-    else:
-        # if the called key does not exist or contain a value
-        # return default value
-        color_value = default_color_value
-
-    return(color_value)
-'''
